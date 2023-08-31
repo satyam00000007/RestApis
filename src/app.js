43d2429efb02f4b1e93110ec0c0e4163aaf84500
Shivam Sharma <7610shivam@gmail.com>
2023-08-31 12:29:45 +0530

    create student model

--- src/app.js
+++ src/app.js
@@ -12,13 +12,69 @@ app.get("/",(req,res)=>{
     res.send("hello from other side")
 });
 
-app.post("/students",(req,res)=>{
+app.post("/students",async(req,res)=>{
     const user = new studentModel(req.body);
-    user.save().then(()=>{
-        res.status(201).send(user);
-    }).catch((e)=>{
-        res.status(400).send(e);
-    })
+    // user.save().then(()=>{
+    //     res.status(201).send(user);
+    // }).catch((e)=>{
+    //     res.status(400).send(e);
+    // })
+
+    try{
+        const createUser= await user.save();
+        return res.status(201).send(createUser);
+    }catch(e){
+        return res.status(400).send(e);
+    }
+});
+
+app.get("/list",async(req,res)=>{
+    try{
+        const userList= await studentModel.find();
+        return res.status(201).send(userList);
+    }catch(e){
+        return res.status(400).send(e);
+    }
+});
+
+app.get("/student/:id",async(req,res)=>{
+    try{
+        let _id = req.params.id;
+        const userList= await studentModel.findById({_id});
+        if(!userList){
+            return res.status(404).send();
+        }else{
+            return res.status(201).send(userList);
+        }
+    }catch(e){
+        return res.status(500).send(e);
+    }
+});
+
+app.delete("/student/:id",async(req,res)=>{
+    try{
+        let _id = req.params.id;
+        const userList= await studentModel.findByIdAndDelete({_id});
+        if(!_id){
+            return res.status(404).send();
+        }
+        res.status(201).send(userList);
+    }catch(e){
+        return res.status(500).send(e);
+    }
+});
+
+app.patch("/student/:id",async(req,res)=>{
+    try{
+        let _id = req.params.id;
+        const userList= await studentModel.findByIdAndUpdate({_id}, req.body,{new:true});
+        if(!_id){
+            return res.status(404).send();
+        }
+        res.status(201).send(userList);
+    }catch(e){
+        return res.status(500).send(e);
+    }
 });
 
 app.listen(port,()=>{
